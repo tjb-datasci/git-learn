288a24dfc753863b6cbade36d4dcfd5a0f9c46ab
tjbeaulieu <tjbeaulieu@gmail.com>
2020-04-14 11:46:08 -0400

    reset twice

--- first.py
+++ first.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 x = np.linspace(-10, 10)
 print(x)
+<<<<<<< HEAD
 
 x_log = np.log(x).dropna()
 print(x_log)
 
 def sigmoid(z):
     return = 1 / (1 + np.e**-z)
+=======
+>>>>>>> parent of d1070c9... small changes
--- second.py
+++ second.py
@@ -1,6 +1,3 @@
 import numpy as np
-import matplotlib.pyplot as plt
-x = np.random.randint(10)
-y = np.random.randint(10)
 
-plt.plot(x, y)
+x = np.random.randint(10)
