39eafcd7ef139f2a47ae70877367391bccb29f08
tjbeaulieu <tjbeaulieu@gmail.com>
2020-04-14 13:45:50 -0400

    added statement

--- first.py
+++ first.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 x = np.linspace(-10, 10)
 print(x)
-<<<<<<< HEAD
 
 x_log = np.log(x).dropna()
 print(x_log)
 
 def sigmoid(z):
     return = 1 / (1 + np.e**-z)
-=======
->>>>>>> parent of d1070c9... small changes
+
+print('i made a change')
